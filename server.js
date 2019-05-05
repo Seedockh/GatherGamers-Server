d6c7c7c66b26ea23afeb3fe10eea7a5a5d52dff7
Pierre Hérissé <pierre.herisse@gmail.com>
2019-05-05 20:57:30 +0200

    updated tests, making them truly reliable

--- server.js
+++ server.js
@@ -1,42 +1,19 @@
 import express from "express";
-import bodyParser from 'body-parser';
 import mysql from 'mysql';
-import api from './routes/';
-import { db } from './database/initdb';
-import passport from "passport";
-import cors from "cors";
-import "./middleware/passport";
 import iconv from 'iconv-lite';
 import encodings from 'iconv-lite/encodings';
+import app from './app';
+import { db } from './database/initdb';
+import "./middleware/passport";
 
 iconv.encodings = encodings;
 require('dotenv').config();
 const port = process.env.PORT;
-const app = express();
 
-const start = async () => {
-  try {
-    if (process.env.NODE_ENV) {
-      await db.sync({ force: false });
-    } else {
-      throw new Error('CONFIG ERROR : Please specify your NODE_ENV in an env file')
-    }
+if (process.env.NODE_ENV) {
+  db.sync({ force: true });
+} else {
+  throw new Error('CONFIG ERROR : Please specify your NODE_ENV in an env file')
+}
 
-    app.use(cors());
-    app.use(passport.initialize());
-    app.use(bodyParser.urlencoded({ extended: true }))
-    app.use(bodyParser.json());
-
-    app.get("/", (request, response) => {
-      response.send("Please feel free to use our api with /api");
-    });
-
-    app.use("/api", api);
-  } catch (err) {
-    console.log(err.message);
-  }
-};
-
-start();
-
-module.exports = app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));
--- tests/api.spec.js
+++ tests/api.spec.js
@@ -1,22 +1,22 @@
-let request = require('supertest');
 const api = require('../routes');
-//const app = require('../server');
+const app = require('../app');
+let request = require('supertest');
 
 // TESTS THE API
-describe('Testing the API', ()=>{
-
+describe('Test the API', ()=>{
   test('API Router should be defined', () => {
     expect(api).toBeDefined();
   });
   test('API Router should be an object', () => {
     expect(typeof api).toBe("object");
   });
-
-/*
-  test('API root path should be accessible', async (done) => {
-    request = request('http://localhost:3000/api');
-    await request.get('/').expect(200,done);
-    done();
+  test('API root path should return 200', () => {
+    return request(app).get('/').expect(200);
   });
-*/
-})
+  test('API /api endpoint should return 200', () => {
+    return request(app).get('/api').expect(200);
+  });
+  /*test("API /api/auth should return 401 'Unauthorized'", () => {
+    return request(app).get('/api/auth').expect(401);
+  });*/
+});
