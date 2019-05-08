56471dfc2611706fbbe1df94ead3af54bdc0d9e4
Pierre Hérissé <pierre.herisse@gmail.com>
2019-05-08 15:43:06 +0200

    fixed mailing

--- routes/mailgun.js
+++ routes/mailgun.js
@@ -1,41 +1,22 @@
-import https from 'https';
-const querystring = require('querystring');
+const request = require('request');
 const express = require('express');
 const api = express();
 
 // Send mail to specified email
-api.post('/sendmail', (req,res)=> {
-  const postdata = querystring.stringify({
-    'from' : 'postmaster@sandboxd2f559ffef674d5e8813969f669c8e5d.mailgun.org',
-    'to' : "'"+req.body.useremail+"'",
-    'subject' : 'Hello',
-    'text' : 'Testing some Mailgun awesomeness!'
-  });
-
-  const options = {
-      auth: `api:${process.env.MAILGUN_API_KEY}`,
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Content-Length': Buffer.byteLength(postdata)
-      }
+api.post('/sendmail', async (req,res)=> {
+  const data = {
+    from : 'pierre.herisse@gmail.com', // don't change this address unless you authorize it on mailgun settings
+    to : `${req.body.useremail}`,
+    subject : 'Hello',
+    text : 'Testing some Mailgun awesomeness!'
   };
 
-  const mailrequest = https.request('https://api.mailgun.net/v3/sandboxd2f559ffef674d5e8813969f669c8e5d.mailgun.org/messages', options, (response) => {
-    console.log("======= MAILGUN RESPONSE =======");
-    console.log(`STATUS: ${response.statusCode}`);
-    console.log(`HEADERS: ${JSON.stringify(response.headers)}`);
-    response.setEncoding('utf8');
-    response.on('data', (chunk) => {
-      console.log(`BODY: ${chunk}`);
-    });
-    response.on('end', () => {
-      console.log('No more data in response.');
-    });
-  });
-
-   mailrequest.on('error', (e) => console.error(`problem with request: ${e.message}`) )
-   mailrequest.end();
+  await request.post({
+    url: 'https://api.mailgun.net/v3/sandboxd2f559ffef674d5e8813969f669c8e5d.mailgun.org/messages', form: data },
+  (error, response, body) => {
+    const result = { error: error, status: response && response.statusCode, body: body };
+    res.send(result);
+  }).auth('api',`${process.env.MAILGUN_API_KEY}`);
 })
 
 module.exports = api;
